c4f75fa19544cfb0fc3118120dbe0815e33f4aea
Sebastian Baumann <bauman21@hm.edu>
2017-08-14 17:20:23 +0200

    Updating .gitignore file. Translating comments into english.

--- src/Roman.java
+++ src/Roman.java
@@ -1,54 +1,40 @@
 /**
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 10. November 2016
- * Purpose: Loesung Praktikumseinheit 03: Roman
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
- */
-
-/**
- * Template fuer Klasse Roman.
- *
- * @author Hammerschall
+ * Purpose: solution to lab Roman
  */
 public class Roman {
 
     /**
-     * Liste aller roemischen Zahlen
+     * Variable to store a list of all roman numerals.
      */
     private final String[] roman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
     /**
-     * Arabische Werte zu roemischen Zahlen
+     * Variable to store the arabic correspondants to the roman numerals above.
      */
     private final int[] arabic = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
 
     /**
-     * Berechnet die roemische Darstellung zu einer arabischen Zahl.
-     * @param value arabische Zahl.
-     * @return roemische Darstellung als String.
+     * Method to translate the roman correspondant to an arabic number.
+     * @param value - an arabic number.
+     * @return - returns the roman representation of an arabic number as a string.
      */
     public String getRoman(final int value) {
 
-        // Variable fuer roemische Darstellung
+        // Variable to store the roman representation.
         String result = "";
 
-        // --------------------------------------
-
-        // Ergaenzen Sie hier Ihren Code
         int newValue = value;
 
-        /**
-         * Aeussere for-Schleife laeuft durch das arabic-Array in fallender
-         * Groesse. Ist das aktuelle Element aus dem arabic-Array in der eingegebenen
-         * Zahl enthalten, wird mittels einer int-Division ein Zaehler gesetzt.
-         * Dieser Zaehler dient dazu in der zweiten for-Schleife die
-         * entsprechenden roemischen Ziffern zum String "result" hinzuzufuegen.
-         * Nachdem das ganze arabic-Array durchlaufen ist, wird die aeussere for-Schleife
-         * beendet und der String "result" zurueckgegeben.
+        /*
+         * This loop iterates through the 'arabic' array. Everytime a value from the 'arabic' array is contained
+         * in the arabic number, than a counter 'counter' is incremeneted. In the second loop the corresponding
+         * string representation of the 'roman' array is added 'counter' times to the string 'result'. If the whole
+         * 'arabic' array processed, than the 'result' string is returned.
          */
         for (int i = roman.length - 1; i >= 0; i--) {
 
@@ -62,46 +48,34 @@ public class Roman {
             }
         }
 
-        // ---------------------------------------
-
         return result;
 
     }
 
     /**
-     * Start des Programms Roman.
-     * @param args int-Wert zur Umwandlung in roemische Zahl
+     * Main method of the program roman.
+     * @param args - variable number of values from the terminal.
      */
     public static void main(String[] args) {
 
-        // Wert fuer positiven Integer einlesen
+        // This imports the arabic number from the terminal.
         int value = Integer.parseInt(args[0]);
 
-        // Pruefung ob Eingabewert <= 0. In diesem Fall
-        // Abbruch des Programms mit Fehlermeldung.
-
-        // --------------------------------------
-
-        /**
-         * Zuerst wird geprueft, ob der Eingabewert kleiner oder gleich 0 ist.
-         * Ist dem so, wird das Programm nicht weiter ausgefuehrt. Nur wenn keine
-         * 0 eingegeben wurde wird im else-Teil des if-else die Methode getRoman()
-         * mit dem eingegebenen Wert aufgerufen.
+        /*
+         * Check, if the arabic number from the console is 0. If so, than an error message
+         * is printed to the screen.
          */
         if (value <= 0) {
 
-            System.out.println("Invalid Input");		// Ausgabe der Fehlermeldung
+            System.out.println("Invalid Input");
 
         }
         else {
 
-            // Aufruf der Methode zur Berechnung der roemischen Zahl.
             String result = new Roman().getRoman(value);
             System.out.println(result);
 
         }
-
-        // ---------------------------------------
 
     }
 
